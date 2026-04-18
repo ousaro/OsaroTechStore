f037f2537d310c5e35f221ebd47dfbd30d762ecf
Ousaro <oussamaouldrhila4@gmail.com>
2026-04-18 20:40:09 +0100

    refactor(backend): move categories wiring into composition module

---
+++ backend/src/modules/categories/composition.js
@@ -0,0 +1,33 @@
+import { buildGetAllCategoriesUseCase } from "./application/use-cases/getAllCategoriesUseCase.js";
+import { buildAddNewCategoryUseCase } from "./application/use-cases/addNewCategoryUseCase.js";
+import { buildDeleteCategoryUseCase } from "./application/use-cases/deleteCategoryUseCase.js";
+import { createCategoriesInputPort } from "./ports/input/categoriesInputPort.js";
+import { createMongooseCategoryRepository } from "./infrastructure/repositories/mongooseCategoryRepository.js";
+import { productsInputPort } from "../products/index.js";
+import { createCategoriesHttpController } from "./infrastructure/http/categoriesHttpController.js";
+
+const categoryRepository = createMongooseCategoryRepository();
+
+const getAllCategoriesUseCase = buildGetAllCategoriesUseCase({
+  categoryRepository,
+});
+const addNewCategoryUseCase = buildAddNewCategoryUseCase({
+  categoryRepository,
+});
+const deleteCategoryUseCase = buildDeleteCategoryUseCase({
+  categoryRepository,
+  removeProductsByCategory: productsInputPort.removeProductsByCategory,
+});
+export const categoriesInputPort = createCategoriesInputPort({
+  getAllCategories: getAllCategoriesUseCase,
+  addNewCategory: addNewCategoryUseCase,
+  deleteCategory: deleteCategoryUseCase,
+});
+
+export const {
+  getAllCategoriesHandler,
+  addNewCategoryHandler,
+  deleteCategoryHandler,
+} = createCategoriesHttpController({
+  categoriesInputPort,
+});
