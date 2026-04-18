627bcaf6eec584b69996296f51a47edc51887eb3
Ousaro <oussamaouldrhila4@gmail.com>
2026-04-18 20:42:29 +0100

    refactor(backend): move payments wiring into composition module

---
+++ backend/src/modules/payments/composition.js
@@ -0,0 +1,32 @@
+import { env } from "../../config/env.js";
+import { buildCreatePaymentIntentUseCase } from "./application/use-cases/createPaymentIntentUseCase.js";
+import { buildVerifyWebhookUseCase } from "./application/use-cases/verifyWebhookUseCase.js";
+import { buildGetSessionDetailsUseCase } from "./application/use-cases/getSessionDetailsUseCase.js";
+import { createPaymentsInputPort } from "./ports/input/paymentsInputPort.js";
+import { createStripeGateway } from "./infrastructure/gateways/stripeGateway.js";
+import { createPaymentsHttpController } from "./infrastructure/http/paymentsHttpController.js";
+
+const paymentGateway = createStripeGateway({
+  secretKey: env.stripeSecretKey,
+  webhookSecret: env.stripeWebhookSecret,
+});
+
+const createPaymentIntentUseCase = buildCreatePaymentIntentUseCase({
+  paymentGateway,
+  clientUrl: env.clientUrl,
+});
+const verifyWebhookUseCase = buildVerifyWebhookUseCase({ paymentGateway });
+const getSessionDetailsUseCase = buildGetSessionDetailsUseCase({ paymentGateway });
+export const paymentsInputPort = createPaymentsInputPort({
+  createPaymentIntent: createPaymentIntentUseCase,
+  verifyWebhook: verifyWebhookUseCase,
+  getSessionDetails: getSessionDetailsUseCase,
+});
+
+export const {
+  createPaymentIntentHandler,
+  stripeWebhookHandler,
+  getSessionDetailsHandler,
+} = createPaymentsHttpController({
+  paymentsInputPort,
+});
